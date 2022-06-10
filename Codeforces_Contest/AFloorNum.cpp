1954d78e73f943b0c435026e69c80e941f66447f
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-06-10 23:59:05 +0530

    A_PlatformNOtworking

---
+++ Codeforces_Contest/AFloorNum.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define int long long 
+using namespace std;
+void begin(){
+	int n,x;
+	cin>>n>>x;
+	if(n==1 || n==2)
+	{
+		cout<<1<<"\n"; return;
+	}
+	int f=1;
+	while(true)
+	{
+		if(x+f<=n && (x+f>=2x+2)){
+			
+		}
+	}
+	cout<<ct<<"\n";
+	return;
+}
+int32_t main()
+{
+	int t=1;
+	cin>>t;
+	while(t--)
+	{
+		begin();
+	}
+	return 0;
+}
