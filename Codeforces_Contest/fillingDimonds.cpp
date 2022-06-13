14720ccf32877c45c28447b772bf9178fc92f51a
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-06-13 23:26:29 +0530

    FillingDIamond CF

---
+++ Codeforces_Contest/fillingDimonds.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define int long long 
+using namespace std;
+void begin(){
+	//Write your code here
+    int n; cin>>n;
+    cout<<n;
+    cout<<"\n";
+	return;
+}
+int32_t main()
+{
+	int t=1;
+	cin>>t;
+	while(t--)
+	{
+		begin();
+	}
+	return 0;
+}
+
