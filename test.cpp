84179d70faf702370b7e76f21669661df8621d56
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-06-15 23:43:56 +0530

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,29 +1,70 @@
-#include<iostream>
-#define e endl
-// #define m INT_MIN
-using namespace std;
-int print(int *a,int i,int key)
-{
-	if(i==0){
-		if(a[0]==key){
-			return 0;
-		}
-		return -1;
-	}
-	int p=print(a,i-1,key);
-	if(p==-1)
-	{
-		if(a[i]==key)
-			return i;
-		else return -1;
-	}
-	return p;
-
-}
-int main()
-{
-	// int n; cin>>n;
-	int a[5]={2,5,11,6,89};
-	cout<<print(a,4,89);
-	return 0;
-}
+    // Problem: G. 2^Sort
+    // Contest: Codeforces - Codeforces Round #799 (Div. 4)
+    // URL: https://codeforces.com/contest/1692/problem/G
+    // Memory Limit: 256 MB
+    // Time Limit: 1000 ms
+     
+    #include<bits/stdc++.h>
+    #define int long long int
+    #define ld long double
+    #define all(c) c.begin(),c.end()
+    #define lb lower_bound
+    #define ub upper_bound
+    #define mp(a,b) make_pair(a,b)
+    #define pb(c) push_back(c)
+    #define mod 1000000007
+    #define NITRO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+     
+    using namespace std;
+     
+    void code()
+    {
+        /*Start your code here*/
+        int n,k; cin>>n>>k;
+        vector<int> arr(n);
+        for(auto &i:arr) cin>>i;
+        k++;
+        int i=-1,j=-1;
+        int res=0;
+        while(true)
+        {
+            bool f1=true,f2=true;
+            while(i<n-1)
+            {
+                i++;
+                f1=false;
+                if(i>0 && 2*arr[i] <= arr[i-1])
+                {
+                    j=i-1;
+                }
+                if(i-j==k)
+                {
+                    res++;
+                    break;
+                }
+            }
+            while(j<i)
+            {
+                j++;
+                break;
+            }
+            if(f1&&f2) break;
+        }
+        cout<<res;
+        cout<<"\n";
+    }
+     
+    int32_t main()
+    {
+        /*freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);*/
+     
+        NITRO
+        int t=1;
+        cin>>t;
+        while(t--)
+        {
+            code();
+        }
+        return 0;
+    }
