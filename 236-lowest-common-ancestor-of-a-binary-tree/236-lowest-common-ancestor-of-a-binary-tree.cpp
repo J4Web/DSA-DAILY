56b0c7576a409080961a1a632987a5cb0cb41226
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-07-27 01:46:50 +0530

    Time: 29 ms (35.14%), Space: 14.3 MB (25.30%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,10 +9,9 @@
  */
 class Solution {
 public:
-    TreeNode* helper(TreeNode* root, TreeNode* p,TreeNode* q)
+    TreeNode* helper(TreeNode* root,TreeNode* p, TreeNode* q)
     {
-        if(root==nullptr) return nullptr;
-        if(root==p || root==q) return root;
+        if(!root || root==p ||  root==q) return root;
         TreeNode* left=helper(root->left,p,q);
         TreeNode* right=helper(root->right,p,q);
         if(left && right) return root;
