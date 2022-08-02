e69d36e2eb7c962a68faf42a91fc0ac68f53a016
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-03 00:13:35 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.85%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int helper(int n,vector<int> &dp)
-    {
-        if(n==0 || n==1)
-        {
-            dp[n]=n;
-            return n;
-        }
-        if(dp[n]!=-1) return dp[n];
-       return dp[n]=helper(n-1,dp)+helper(n-2,dp);
-        
-    }
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return helper(n,dp);
+        if(n==0 || n==1) return n;
+        int cur=0,prev1=0,prev2=0;
+        prev1=1;
+        prev2=0;
+        for(int i=2;i<=n;i++)
+        {
+            cur=prev1+prev2;
+            prev2=prev1;
+            prev1=cur;
+        }
+        return cur;
     }
 };
