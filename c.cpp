39445431359dfe0c88b7d42cc7c32f1b51ae24f4
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-17 23:52:50 +0530

    Cf B problem update/Lazy day/DEPRESSED DAY.

--- c.cpp
+++ c.cpp
@@ -312,3 +312,26 @@ int main()
   cout<<maxi<<e;
   return 0;
 }
+
+
+//Codeforces B problem
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, a, b;
+    cin >> n >> a >> b;
+    string s;
+    for (int i = 0, j = 0; i < n; i++, j++) {
+      s += (j % b) + 'a';
+    }
+    cout << s << endl;
+  }
+  return 0;
+}
+
