4479cd69088354c2036e5ce97bc19e2a7d5d4a3c
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-18 02:08:25 +0530

    Time: 8 ms (34.91%), Space: 8.8 MB (16.15%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
+        
+        //just make an array to map each lowercase alphabets to its transformation 
         vector<string> s={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        //make a set to get unique transformation
         set<string> st;
+        
+        //looping through each word
         for(auto c:words)
         {
+            //storing the transformed word
             string trans;
+            //getting each char and converting it according to given transformation
             for(auto ch:c)
             {
                 trans+=s[ch-97];
             }
+            //pushing it int set
             st.insert(trans);
             
         }
+        //returning unique transformations
         return st.size();
     }
 };
