65e6b6a5be2f7f4dc2c6aa045fcafaaebcadc6cb
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-24 21:36:24 +0530

    Time: 20 ms (32.70%), Space: 7.3 MB (46.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> st;
+        map<char,int> mp;
+        map<char,int> mp1;
         if(s.size()!=t.size()) return false;
-        for(auto i:s) st[i]++;
-        map<char,int> st1;
-        for(auto i:t) st1[i]++;
-        
         for(auto i:s)
         {
-            if(st[i]!=st1[i])
-            { 
-                return false;
-                
-            }
+            mp[i]++;
+        }
+        for(auto i:t)
+        {
+            mp1[i]++;
+        }
+        for(auto i:s)
+        {
+            if(mp[i]!=mp1[i]) return false;
         }
         return true;
-            
-        
     }
 };
