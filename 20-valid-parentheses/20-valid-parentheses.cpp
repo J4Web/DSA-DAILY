7ad829fed70c286e69cfa9fa4aa7287de82d2f10
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-25 13:09:24 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.24%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        int i=0;
-        while(i<s.size())
+       stack<char> st;
+        for(auto i:s)
         {
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
-            {
-                st.push(s[i]);
-            }
+            if(i=='(' || i=='[' || i=='{') st.push(i);
             else{
-                if(st.size()==0)
-                {
-                    return false;
-                }
-                if(s[i]==')' && st.top()=='(')
-                {
-                    st.pop();
-                
-                }
-                else if(s[i]=='}' && st.top()=='{')
-                {
-                    st.pop();
-                
-                }
-                else if(s[i]==']' && st.top()=='[')
-                {
-                     st.pop();
-                
-                }
-                else {
-                    return false;
-                }
+                if(st.size()==0) return false;
+                else if(st.top()=='(' && i==')' ) st.pop();
+                else if(st.top()=='{'  &&  i=='}') st.pop();
+                else if(st.top()=='[' && i==']' ) st.pop();
+                else return false;
             }
-             i++;
         }
-        if(st.size()==0)
-        {
-            return true;
-        }
+        if(st.size()==0) return true;
         return false;
     }
 };
