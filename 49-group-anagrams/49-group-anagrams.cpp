8708b373b7ff332212193e9783edf724c3a8e79c
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-25 21:58:42 +0530

    Time: 78 ms (28.57%), Space: 20.8 MB (34.53%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<map<char,int>,vector<string>> bmap;
         vector<vector<string>> res;
-        int  n=strs.size();
+        map<string,vector<string>> mp;
         for(auto i:strs)
         {
-            //freq map for each element
-            map<char,int> mp;
-            string str=i;
-            //finding freq
-            for(auto j:str)
-            {
-                mp[j]++;
-            }
-            //checking if such freq map is there in our map already or not
-            //if present then eh we take out the vector and push in the same thing if not then we create one
-            // and create its freq vector mapping
-            if(bmap.find(mp)==bmap.end())
-            {
-                vector<string> newVec;
-                newVec.push_back(i);
-                bmap.insert({mp,newVec});
-            }
-            else{
-                vector<string> prev=bmap[mp];
-                prev.push_back(str);
-                bmap[mp]=prev;
-            }
+            string s=i;
+            sort(s.begin(),s.end());
+            mp[s].push_back(i);
+            
         }
-        for(auto i:bmap)
+        for(auto i:mp)
         {
             res.push_back(i.second);
         }
