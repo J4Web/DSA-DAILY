cc4b2c9c34af01bf4871d20e8971011cdc9afe2a
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-25 18:37:42 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (62.03%) - LeetHub

---
+++ 791-custom-sort-string/791-custom-sort-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        map<char,int> mp;
+        for(auto i:s)
+        {
+            mp[i]++;
+        }
+        string res="";
+        for(auto i:order){
+            res+=string(mp[i],i);
+            mp[i]=0;
+        }
+       for(auto i:mp)
+       {
+           res+=string(i.second,i.first);
+           
+       }
+        
+        return res;
+    }
+};
