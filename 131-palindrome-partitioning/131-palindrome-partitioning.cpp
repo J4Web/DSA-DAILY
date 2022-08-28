b9af08a3f3b113719b71fa9732e3afc4e69aa2a1
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-28 12:01:19 +0530

    Time: 191 ms (61.36%), Space: 49.3 MB (81.94%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPalindrome(string s)
+    {
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j)
+        {
+            if(s[i]!=s[j]){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    void solution(string s, vector<vector<string>> &res,vector<string> &asf)
+    {
+        if(s.size()==0){
+            res.push_back(asf);
+            return;
+        }
+        for(auto i=0;i<s.size();i++)
+        {
+            string prefix=s.substr(0,i+1);
+            string ros=s.substr(i+1);
+            if(isPalindrome(prefix)){
+                asf.push_back(prefix);
+                solution(ros,res,asf);
+                asf.pop_back();
+            } 
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> asf;
+        solution(s,res,asf);
+        return res;
+    }
+};
