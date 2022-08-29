9d242cf12f7edd8b4eb2939fa212d2ff094d014c
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-29 23:58:22 +0530

    new cf sol

--- c.cpp
+++ c.cpp
@@ -335,3 +335,29 @@ int main() {
   return 0;
 }
 
+//1506A 
+//Cf 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+  while (t--){
+      long long int n, m, x;
+      cin >> n >> m >> x;
+      long long a = (x - 1) % n, b = (x - 1) / n;
+      cout << a * m + b + 1 << "\n";
+  }
+  return 0;
+
+}
