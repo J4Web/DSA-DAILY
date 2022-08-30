89cb15c13369c3e93afef3986493d9304f188da0
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-30 23:53:48 +0530

    A cp contest

--- Codeforces_Contest/theClock.cpp
+++ Codeforces_Contest/theClock.cpp
@@ -72,3 +72,59 @@ int32_t main()
     }
     return 0;
 }
+
+
+//Div 4 A 
+
+
+#include<bits/stdc++.h>
+#define int long long int
+#define ld long double
+#define all(c) c.begin(),c.end()
+#define lb lower_bound
+#define ub upper_bound
+#define mp(a,b) make_pair(a,b)
+#define pb(c) push_back(c)
+#define mod 1000000007
+#define NITRO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+void code()
+{
+
+      int len; cin>>len;
+    string name="Timur";
+  sort(name.begin(),name.end());
+    string s; cin>>s;
+    sort(s.begin(),s.end());
+  if(name.size()!=s.size()){
+    cout<<"NO"<<"\n";
+    return;
+  }
+    for(auto i=0;i<name.size();i++)
+      {
+        if(name[i]!=s[i]){
+          cout<<"NO"<<"\n";
+          return;
+        }
+        
+      }
+    cout<<"YES"<<"\n";
+  return;
+  
+}
+
+int32_t main()
+{
+
+
+  NITRO
+  int t=1;
+  cin>>t;
+    while(t--)
+    {
+        code();
+    }
+    return 0;
+}
