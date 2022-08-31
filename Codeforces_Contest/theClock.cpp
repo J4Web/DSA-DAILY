d49bb784319ac8b650c32df4877526d3bc52d429
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-08-31 23:51:32 +0530

    cf streak

--- Codeforces_Contest/theClock.cpp
+++ Codeforces_Contest/theClock.cpp
@@ -183,3 +183,30 @@ int32_t main()
     }
     return 0;
 }
+
+
+//999B CF 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  string t;
+  cin >> t;
+  vector<int> divisors;
+  for (int i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      divisors.push_back(i);
+    }
+  }
+  string s = t;
+  for (int i = 0; i < divisors.size(); i++) {
+    for (int j = 0; j < divisors[i] / 2; j++) {
+      swap(s[j], s[divisors[i] - 1 - j]);
+    }
+  }
+  cout << s << endl;
+  return 0;
+}
