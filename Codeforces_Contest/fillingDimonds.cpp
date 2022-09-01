3c368f047d1e00c57eab2a324b23b0cd4b0b4906
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-09-01 23:52:42 +0530

    D done

--- Codeforces_Contest/fillingDimonds.cpp
+++ Codeforces_Contest/fillingDimonds.cpp
@@ -19,3 +19,61 @@ int32_t main()
 	return 0;
 }
 
+
+//Div4 817 B - LINE
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void begin()
+{
+  //start you code here
+  int n; cin>>n;
+  string s; cin>>s;
+  int score=0;
+  for(int i=0;i<n;++i)
+  {
+    if(s[i]=='L')
+    {
+      score+=i;
+    }
+    else{
+      score+=(n-i-1);
+    }
+  }
+
+  priority_queue<int> q;
+  for(int i=0;i<n;++i)
+  {
+    if(s[i]=='L') q.push(((n-i-1)-i));
+
+    else q.push(i-((n-i-1)));
+
+  }
+
+
+  for(int k=1;k<=n;++k)
+  {
+    int tp=q.top();
+    q.pop();
+    if(tp>0)
+    {
+      score+=tp;
+    }
+    cout<<score<<" ";
+  }
+  cout<<"\n";
+
+  return;
+}
+
+int32_t main() {
+  int t=1;
+  cin>>t;
+  while(t--)
+  {
+      begin();
+  }
+
+  
+  return 0;
+}
