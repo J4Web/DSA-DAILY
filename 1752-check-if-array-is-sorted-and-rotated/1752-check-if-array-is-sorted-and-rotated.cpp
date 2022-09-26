beac3aedfb9023193765a1ecdf0a6c3a1e081bc6
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-09-26 23:45:52 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (92.95%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int idx=-1;
+        for(int i=0;i<nums.size()-1;++i)
+        {
+            if(nums[i]<=nums[i+1])
+            {
+                continue;
+            }
+            else{
+                idx=i+1;break;
+            }
+        }
+        if(idx==-1 || idx==nums.size()) return true;
+        cout<<"idx:"<<idx<<"\n";
+        bool res1=true;
+        for(int i=0;i<idx-1;++i)
+        {
+            if(nums[i]>nums[i+1])
+            {
+                cout<<"at ith we see : "<<i<<"\n";
+                res1=false;
+                break;
+            }
+        
+        }   
+        bool res2=true;
+        for(int i=idx;i<nums.size()-1;++i)
+        {
+            if(nums[i]>nums[i+1]){
+                res2=false;
+                break;
+            }
+        }
+        bool res3=true;
+        if(nums[nums.size()-1]>nums[0]) res3=false;
+        return (res1&&res2 && res3);
+    }
+};
