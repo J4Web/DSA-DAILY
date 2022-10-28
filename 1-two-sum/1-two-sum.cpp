dbb0b6999d4decb60f322bc440e5bab4b4ae002c
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2022-10-28 10:28:11 +0530

    Time: 29 ms (59.71%), Space: 11.1 MB (22.13%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int> res;
+        int l=0;
+        vector<pair<int,int>> arr;
+        for(auto i=0;i<n;i++)
+        {
+            arr.push_back({nums[i],i});
+        }
+         sort(arr.begin(),arr.end());
+        int r=n-1;
+        for(;l<r;)
+        {
+            int sum=arr[l].first+arr[r].first;
+            if(sum==target)
+            {
+                res.push_back(arr[l].second);
+                res.push_back(arr[r].second);
+                l++;
+                r--;
+                break;
+            }
+            if(sum>target) r--;
+                
+            if(sum<target) l++;
+        }
+        return res;
+        
+    }
+};
