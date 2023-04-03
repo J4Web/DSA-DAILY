269fa52f6404b898418f529f4efc25edfbbd2eae
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2023-04-03 08:41:49 +0530

    Time: 23 ms (12.50%), Space: 30.3 MB (12.50%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums){
+        //result vector
         vector<vector<int>> arr;
+        //map to keep track of frequency 
         unordered_map<int,int> mp;
+        //counting frequency 
         for(auto i:nums) mp[i]++;
-            
-        vector<int> key_to_del;
-        
-       while(mp.size()>0)
+        //filling up the arr vector until the map becomes empty    
+        while(mp.size()>0)
         {
             vector<int> res;
-            for(auto it=mp.begin();it!=mp.end();++it)
+            vector<int> key_to_del;
+            for(auto& [key, count]: mp)
             {
-
-                    res.push_back(it->first);
-                    mp[it->first]--;
-                   if(mp[it->first]==0) key_to_del.push_back(it->first);
-
+                res.push_back(key);
+                count--;
+                if(count==0) key_to_del.push_back(key);
             }
-           for(auto i:key_to_del)
-           {
-               mp.erase(i);
-           }
+            for(auto i:key_to_del) mp.erase(i);
             arr.push_back(res);
         }
 
